a16f951fdc0bd27b8998278f1665c8409486f4c7
Shvetam <shvetam@sixergame.com>
2023-05-18 18:12:10 +0530

    initial commit

---
+++ src/reducers/addfavReducer.ts
@@ -0,0 +1,10 @@
+const INITIAL_STATE = [];
+
+export const favProductsReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'ADD_FAV':
+      return;
+    default:
+      return;
+  }
+};
---
+++ src/reducers/productsReducer.ts
@@ -0,0 +1,10 @@
+const INITIAL_STATE = [];
+
+export const productsReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SET_PRODUCTS':
+      return;
+    default:
+      return;
+  }
+};
